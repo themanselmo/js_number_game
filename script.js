82ac37eb67724770c5b82c0c1af946c2b96065c7
Max Anselmo <itmanselmo@gmail.com>
2021-03-05 17:42:43 -0500

    Added an alert message if the player enters a number outside of the correct bounds, also added comments for each method and variable.

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
+// the human player score
 let humanScore = 0;
+
+// the computer player score
 let computerScore = 0;
+
+// the current round number
 let currentRoundNumber = 1;
 
-// Write your code below:
+// generates a random target number for the players 
+//  to guess
 let generateTarget = () => {
   return Math.floor(Math.random() * 9);
 }
 
+// compares the guesses of the human and computer 
+// players, checking to make sure that the human players
+// guessed number is within the appropriate bounds
 let compareGuesses = (human, comp, target) => {
-  let humanDiff = (target - human);
-  let compDiff = Math.abs(target - comp);
-  if(humanDiff === compDiff){
-    return true;
-  } else if(humanDiff < compDiff){
-    return true;
+  if(human > 9 || human < 0){
+    alert('Please enter a number between 0 and 9.');
   } else {
-    return false;
+    let humanDiff = (target - human);
+    let compDiff = Math.abs(target - comp);
+    if(humanDiff === compDiff){
+      return true;
+    } else if(humanDiff < compDiff){
+      return true;
+    } else {
+      return false;
+    }
   }
+  
 }
 
+// updates the score on the page depending on the 
+//  winner of each round
 let updateScore = (winner) => {
   if(winner === 'human'){
     humanScore++;
@@ -29,7 +45,8 @@ let updateScore = (winner) => {
   }
 }
 
+// increments the current round number by 1 at the end
+//  of the round
 let advanceRound = () => {
   currentRoundNumber++;
 }
-
